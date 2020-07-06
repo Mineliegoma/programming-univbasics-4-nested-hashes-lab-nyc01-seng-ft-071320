417c682300d55d4d4886724c1818d6473fd6cc5e
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-06 12:18:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,13 +66,9 @@ def adding_matz
 #   :known_for => "Ruby",
 #   :languages => ["LISP", "C"]
 # }
-#{[:yukihiro_matsumoto]={:known_for => "Ruby", :languages => ["LISP", "C"]}
-#}
 
-#programmer_hash[:yukihiro_matsumoto][:known_for ]= "Ruby" 
-#programmer_hash[:yukihiro_matsumoto][:languages]= ["LISP", "C"]
-programmer_hash={:yukihiro_matsumoto=>{:known_for=>"Ruby", :languages=>["LISP", "C"]}
-  }
+programmer_hash[:yukihiro_matsumoto]={:known_for=>"Ruby", :languages=>["LISP", "C"]}
+  
 # return the entire updated hash
 programmer_hash
 
